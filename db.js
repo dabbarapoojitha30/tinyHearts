c4848af8a73fa346f9b326e6efa6908a0553582b
Poojitha Dabbara <dabbarapoojitha30@gmail.com>
2026-01-29 19:16:16 +0530

    Connect to Render DB

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// Use DATABASE_URL from Render for production
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: false
 });
 
 pool.query('SELECT current_database();')
